e80fef66177e1fde2565666549b92bb8371c41c8
Gautam Singh <goats.singh@gmail.com>
2025-04-29 10:46:06 +0530

    refactor: Organize source and reports

---
+++ assignments/retracing-boomerang/src/main.py
@@ -0,0 +1,16 @@
+"""
+File        : main.py
+Author      : Gautam Singh
+Date        : 2025-04-29
+Description : Implementation of the retracing boomerang attack on secret
+              S-boxes from Dunkelmann et. al. 2019.
+"""
+
+# Imports
+
+# Constants
+
+if __name__ == '__main__':
+    # Instantiate AES Cipher
+
+    # Perform retracing boomerang attack
