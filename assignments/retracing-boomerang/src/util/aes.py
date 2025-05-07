2a89459ef5bb034e4170422847860332b043a213
Gautam Singh <goats.singh@gmail.com>
2025-05-07 15:41:39 +0530

    fix: Implement AES as per FIPS

--- assignments/retracing-boomerang/src/util/aes.py
+++ assignments/retracing-boomerang/src/util/aes.py
@@ -5,7 +5,7 @@ class AES:
     """
     Instantiation of AES cipher from S box and MC.
     """
-    def _check_state(self, state: galois.FieldArray) -> None:
+    def _check_state(self, state) -> None:
         """
         Check if the provided state is a valid field element.
         
@@ -15,10 +15,10 @@ class AES:
         Raises:
             ValueError: If the state is not a valid field element.
         """
-        if not isinstance(state, galois.FieldArray) or state.shape != (4, 4):
-            raise ValueError(f"State {state} must be a galois.FieldArray of shape (4, 4).")
-        if type(state) is not GF_:
-            raise ValueError(f"State {state} must be in the same field as the AES instance.")
+        assert isinstance(state, galois.FieldArray) and state.shape == (4, 4), \
+            ValueError(f"State {state} must be a galois.FieldArray of shape (4, 4).")
+        assert isinstance(state, GF_), \
+            ValueError(f"State {state} must be in the same field as the AES instance.")
     
     def _step_wrapper(self, step: AesStep, state: galois.FieldArray, key: galois.FieldArray, round: int) -> galois.FieldArray:
         """
@@ -68,7 +68,9 @@ class AES:
         # Check if the state is a valid field element
         self._check_state(state)
         # Perform the transformation
-        res = np.roll(state, 1 if inv else -1, axis=1)
+        res = state.copy()
+        for i in range(4):
+            res[i] = np.roll(res[i], i * (1 if inv else -1))
         # Regain type annotation
         res = GF_(res)
         # Check if the result is a valid field element
@@ -81,7 +83,7 @@ class AES:
         mix_columns: AesStep = aes_mix_columns_,
         inv_mix_columns: AesStep = aes_inv_mix_columns_,
         key_schedule: AesKeySchedule = aes_key_schedule_,
-        key: galois.FieldArray | None = None,
+        key: galois.FieldArray | bytes | None = None,
         num_rounds: int = 10
     ) -> None:
         self.s_box_ = s_box
@@ -91,15 +93,47 @@ class AES:
         if key is None:
             # Pick a random master key
             self.key = GF_.Random((4, 4))
-        else:
+        elif type(key) is bytes:
+            # Convert bytes to field element
+            self.key = GF_(np.frombuffer(key, dtype=np.uint8).reshape((4, 4), order='F'))
+        elif type(key) is galois.FieldArray:
             # Use the provided key
             self.key = key
         # Check if the key is a valid field element
         self._check_state(self.key)
         self.num_rounds = num_rounds
         self.key_schedule = key_schedule(self.key, self.num_rounds)
+
+    def encrypt_(self, pt: galois.FieldArray) -> galois.FieldArray:
+        # Get whitening key
+        whitening_key = self.key_schedule[0]
+        # Apply whitening key to plaintext
+        pt = pt + whitening_key
+        
+        # Do rounds 1 to n - 1
+        for i in range(1, self.num_rounds):
+            round_key = self.key_schedule[i]
+            # Apply S-Box transformation
+            pt = self.s_box(pt, round_key, i)
+            # Apply Shift Rows transformation
+            pt = self.shift_rows(pt)
+            # Apply Mix Columns transformation
+            pt = self.mix_columns(pt, round_key, i)
+            # Add round key
+            pt = pt + round_key
+        
+        # Do the last round (no Mix Columns)
+        round_key = self.key_schedule[-1]
+        # Apply S-Box transformation
+        pt = self.s_box(pt, round_key, self.num_rounds - 1)
+        # Apply Shift Rows transformation
+        pt = self.shift_rows(pt)
+        # Add round key
+        pt = pt + round_key
+        # Return the ciphertext
+        return pt
     
-    def encrypt(self, plaintext: galois.FieldArray) -> galois.FieldArray:
+    def encrypt(self, plaintext: bytes) -> bytes:
         """
         Encrypt the plaintext using the AES cipher.
         
@@ -109,38 +143,41 @@ class AES:
         Returns:
             galois.FieldArray: The ciphertext.
         """
+        pt = GF_(np.frombuffer(plaintext, dtype=np.uint8).reshape((4, 4), order='F'))
         # Check if the plaintext is a valid field element
-        self._check_state(plaintext)
-        
-        # Get whitening key
-        whitening_key = self.key_schedule[0]
-        # Apply whitening key to plaintext
-        plaintext = plaintext + whitening_key
-        
-        # Do rounds 1 to n - 1
-        for i in range(1, self.num_rounds - 1):
-            round_key = self.key_schedule[i]
-            # Apply S-Box transformation
-            plaintext = self.s_box(plaintext, round_key, i)
-            # Apply Shift Rows transformation
-            plaintext = self.shift_rows(plaintext)
-            # Apply Mix Columns transformation
-            plaintext = self.mix_columns(plaintext, round_key, i)
-            # Add round key
-            plaintext = plaintext + round_key
-        
-        # Do the last round (no Mix Columns)
-        round_key = self.key_schedule[-1]
-        # Apply S-Box transformation
-        plaintext = self.s_box(plaintext, round_key, self.num_rounds - 1)
-        # Apply Shift Rows transformation
-        plaintext = self.shift_rows(plaintext)
-        # Add round key
-        plaintext = plaintext + round_key
-        # Return the ciphertext
-        return plaintext
+        self._check_state(pt)
+        return self.encrypt_(pt).flatten(order='F').tobytes()
 
-    def decrypt(self, ciphertext: galois.FieldArray) -> galois.FieldArray:
+    def decrypt_(self, ct: galois.FieldArray) -> galois.FieldArray:
+        # Get whitening key
+        whitening_key = self.key_schedule[-1]
+        # Apply whitening key to ct
+        ct = ct + whitening_key
+        
+        # Do rounds n - 1 to 1
+        for i in range(self.num_rounds - 1, 0, -1):
+            round_key = self.key_schedule[i]
+            # Apply Inverse Shift Rows transformation
+            ct = self.shift_rows(ct, inv=True)
+            # Apply Inverse S-Box transformation
+            ct = self.inv_s_box(ct, round_key, i)
+            # Add round key
+            ct = ct + round_key
+            # Apply Inverse Mix Columns transformation
+            ct = self.inv_mix_columns(ct, round_key, i)
+        
+        # Do the last round (no Inverse Mix Columns)
+        round_key = self.key_schedule[0]
+        # Apply Inverse Shift Rows transformation
+        ct = self.shift_rows(ct, inv=True)
+        # Apply Inverse S-Box transformation
+        ct = self.inv_s_box(ct, round_key, 0)
+        # Add round key
+        ct = ct + round_key
+        # Return the plaintext
+        return ct
+
+    def decrypt(self, ciphertext: bytes) -> bytes:
         """
         Decrypt the ciphertext using the AES cipher.
         
@@ -152,33 +189,7 @@ class AES:
         Returns:
             galois.FieldArray: The plaintext.
         """
+        ct = GF_(np.frombuffer(ciphertext, dtype=np.uint8).reshape((4, 4), order='F'))
         # Check if the ciphertext is a valid field element
-        self._check_state(ciphertext)
-        
-        # Get whitening key
-        whitening_key = self.key_schedule[0]
-        # Apply whitening key to ciphertext
-        ciphertext = ciphertext + whitening_key
-        
-        # Do rounds n - 1 to 1
-        for i in range(self.num_rounds - 1, 0, -1):
-            round_key = self.key_schedule[i]
-            # Apply Inverse S-Box transformation
-            ciphertext = self.inv_s_box(ciphertext, round_key, i)
-            # Apply Inverse Shift Rows transformation
-            ciphertext = self.shift_rows(ciphertext, inv=True)
-            # Apply Inverse Mix Columns transformation
-            ciphertext = self.inv_mix_columns(ciphertext, round_key, i)
-            # Add round key
-            ciphertext = ciphertext + round_key
-        
-        # Do the last round (no Inverse Mix Columns)
-        round_key = self.key_schedule[0]
-        # Apply Inverse S-Box transformation
-        ciphertext = self.inv_s_box(ciphertext, round_key, 0)
-        # Apply Inverse Shift Rows transformation
-        ciphertext = self.shift_rows(ciphertext, inv=True)
-        # Add round key
-        ciphertext = ciphertext + round_key
-        # Return the plaintext
-        return ciphertext
+        self._check_state(ct)
+        return self.decrypt_(ct).flatten(order='F').tobytes()
