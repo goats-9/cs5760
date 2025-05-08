cbdfbe0872b4264ea1f9598a00404fa55d00a9e3
Gautam Singh <goats.singh@gmail.com>
2025-05-08 16:17:58 +0530

    fix: Update yoyo implementation to return the successful initial plaintext pair

--- assignments/retracing-boomerang/src/util/yoyo.py
+++ assignments/retracing-boomerang/src/util/yoyo.py
@@ -1,7 +1,7 @@
 import numpy as np
 import galois
 from .aes import AES
-from .aes_constants import GF_
+from .aes_constants import GF_, rng_
 
 def simple_swap(x: galois.FieldArray, y: galois.FieldArray) -> tuple[galois.FieldArray, galois.FieldArray]:
     """
@@ -26,7 +26,7 @@ def simple_swap(x: galois.FieldArray, y: galois.FieldArray) -> tuple[galois.Fiel
             return x, y
     raise ValueError(f"Inputs to SimpleSWAP must be different, got {x}, {y}.")
 
-def yoyo_distinguisher_5rd(aes: AES) -> bool:
+def yoyo_distinguisher_5rd(aes: AES) -> tuple[bool, galois.FieldArray, galois.FieldArray]:
     """
     Implements the five round distinguisher for the AES cipher using the yoyo
     technique described in this paper:
@@ -36,7 +36,9 @@ def yoyo_distinguisher_5rd(aes: AES) -> bool:
         aes (AES): The AES cipher instance.
     
     Returns:
-        bool: True if the cipher is AES, False otherwise.
+        tuple[bool, galois.FieldArray, galois.FieldArray]: A tuple containing a
+        boolean indicating if the yoyo distinguisher was successful, and the two
+        plaintexts used in the yoyo operation.
     """
     # Ensure the AES instance has 5 rounds
     assert aes.num_rounds == 5, ValueError(f"AES instance must have 5 rounds, but got {aes.num_rounds}.")
@@ -47,30 +49,26 @@ def yoyo_distinguisher_5rd(aes: AES) -> bool:
     cnt1 = 0
     while cnt1 < CNT1:
         cnt1 += 1
-        # Generate random 128-bit plaintext pairs which differ only in one word.
-        # Here, we randomly generate one column for each plaintext and set the
-        # other columns to zero.
-        q0 = GF_.Zeros((4, 4))
-        q1 = GF_.Zeros((4, 4))
-        # Pick a random word that will differ
-        col = np.random.randint(0, 4)
-        q0[:, col] = GF_.Random(4)
-        while True:
-            q1[:, col] = GF_.Random(4)
-            if (q0[:, col] != q1[:, col]).any():
-                break
-        # In the paper, the first SR is stripped, so we apply inverse SR to get
-        # the real plaintexts that should go into the AES cipher.
-        p0 = aes.shift_rows(q0, inv=True)
-        p1 = aes.shift_rows(q1, inv=True)
+        # Generate random 128-bit plaintext pairs which differ only in the first
+        # word.
+        p0 = GF_.Random((4, 4), seed=rng_)
+        p1 = p0.copy()
+        p1[:, 0] = GF_.Random(4, seed=rng_)
+        # Save a copy of the initial plaintexts
+        p0_init = p0.copy()
+        p1_init = p1.copy()
         # Initialize second counter
         cnt2 = 0
         wrong_pair = False
         while cnt2 < CNT2 and wrong_pair == False:
             cnt2 += 1
+            # In the original paper, the first SR is stripped, so to get the
+            # actual plaintexts we apply inverse SR.
+            q0 = aes.shift_rows(p0, inv=True)
+            q1 = aes.shift_rows(p1, inv=True)
             # Encrypt plaintexts
-            d0 = aes.encrypt_(p0)
-            d1 = aes.encrypt_(p1)
+            d0 = aes.encrypt_(q0)
+            d1 = aes.encrypt_(q1)
             # In the original paper, the last SR and MC are stripped, so we
             # apply inverse MC and SR to get the real ciphertexts that should be
             # compared. However, in an actual AES implementation, the last MC is
@@ -79,27 +77,24 @@ def yoyo_distinguisher_5rd(aes: AES) -> bool:
             c1 = aes.shift_rows(d1, inv=True)
             # Perform the SimpleSWAP operation on the ciphertexts
             cc0, cc1 = simple_swap(c0, c1)
+            # The actual ciphertexts are obtained by applying the last SR.
+            dd0 = aes.shift_rows(cc0)
+            dd1 = aes.shift_rows(cc1)
             # Perform decryption on the new ciphertexts to get friend pairs.
-            qq0 = aes.decrypt_(cc0)
-            qq1 = aes.decrypt_(cc1)
+            qq0 = aes.decrypt_(dd0)
+            qq1 = aes.decrypt_(dd1)
             # Apply SR to remove the last inverse SR. Again, the last MC is not
-            # applied in this implementation.
+            # applied in an AES implementation so we don't account for it.
             pp0 = aes.shift_rows(qq0)
             pp1 = aes.shift_rows(qq1)
-            # Get the XOR of the two plaintexts
-            p = pp0 + pp1
-            # Check if there is a word with at least two zeros in the difference
-            for i in range(4):
-                zero_cnt = 0
-                for j in range(4):
-                    zero_cnt += p[j][i] == 0
-                if zero_cnt >= 2:
-                    # If there is, we have a wrong pair, so we break out of the loop
-                    wrong_pair = True
-                    break
+            # Check if there is a word with at least two equal bytes
+            if (np.sum(pp0 == pp1, axis = 0) >= 2).any():
+                # If there is, we have a wrong pair, so we break out of the loop
+                wrong_pair = True
+                break
             # Perform the yoyo by applying SimpleSWAP on the plaintexts
             p0, p1 = simple_swap(pp0, pp1)
         if wrong_pair == False:
             # This is AES
-            return True
-    return False
+            return True, p0_init, p1_init
+    return False, GF_.Zeros((4, 4)), GF_.Zeros((4, 4))
