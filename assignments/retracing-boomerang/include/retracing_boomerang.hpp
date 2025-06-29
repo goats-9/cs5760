2b862f7c5462041c8457267ab9807d5a8b861ff4
Gautam Singh <goats.singh@gmail.com>
2025-06-29 05:46:00 +0000

    docs: Add notes to incomplete implementations

--- assignments/retracing-boomerang/include/retracing_boomerang.hpp
+++ assignments/retracing-boomerang/include/retracing_boomerang.hpp
@@ -15,6 +15,7 @@ namespace boomerang {
     aes_key_t retracing_boomerang_attack(Oracle<block_t, block_t, aes_key_t>&);   
 
     /**
+     * @note This attack is not fully implemented.
      * @brief Performs the retracing boomerang attack of Dunkelman et. al. on AES with secret S-boxes.
      * @param oracle The oracle to use for encryption and decryption.
      * @return The recovered AES key.
@@ -22,9 +23,10 @@ namespace boomerang {
     aes_key_t retracing_boomerang_attack_secret(Oracle<block_t, block_t, aes_key_t>&);
 
     /**
-     *  @brief Performs the retracing boomerang attack of Dunkelman et. al. on AES with secret S-boxes using the yoyo distinguisher of Ronjom et. al.
-     *  @param oracle The oracle to use for encryption and decryption.
-     *  @return The recovered AES key.
+     * @note This attack is not fully implemented.
+     * @brief Performs the retracing boomerang attack of Dunkelman et. al. on AES with secret S-boxes using the yoyo distinguisher of Ronjom et. al.
+     * @param oracle The oracle to use for encryption and decryption.
+     * @return The recovered AES key.
      */
     aes_key_t retracing_boomerang_attack_secret_yoyo(Oracle<block_t, block_t, aes_key_t>&);
 }
